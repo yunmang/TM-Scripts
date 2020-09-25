eeb6674c21f04f5993a243967c701fbf26b8b963
yunmang <yunmang@gmail.com>
2020-09-25 22:19:58 +0800

    Create Unlock.user.js

---
+++ Unlock.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Unlock
+// @namespace    https://github.com/yunmang/TM-Scripts
+// @version      0.1
+// @description  Unlock user-select
+// @author       yunmang
+// @match        http*://*/*
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener('load', function () {
+    // Get all elements that have a style attribute
+    var elms = document.querySelectorAll("*[style]");
+
+    // Loop through them
+    Array.prototype.forEach.call(elms, function(elm) {
+        if (elm.style.userSelect = "none")
+        {
+            elm.style.userSelect = "auto";
+        }
+    });
+})
